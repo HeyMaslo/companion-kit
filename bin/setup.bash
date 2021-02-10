39ae010ee665e759f5ae44914c26b2747719ee12
rlnsy <rowlindsay@gmail.com>
2021-02-10 10:41:36 -0800

    do top-level install and env set in script

--- bin/setup.bash
+++ bin/setup.bash
@@ -11,13 +11,16 @@ else
 		exit 0
 	fi
 fi
-echo "Cloning external dependencies..."
+yarn install
+ls .env || exit 1
+yarn env:set
 mkdir mobile/dependencies
-cd mobile/dependencies
+cd mobile/dependencies || exit 1
 git clone -b dev-ts https://github.com/HeyMaslo/maslo-persona.git persona
 git clone https://github.com/HeyMaslo/react-native-switch-pro.git
 cd ../..
-cd mobile/ios
+yarn all
+cd mobile/ios || exit 1
 pod install --repo-update
 cd ..
 echo "Done."
