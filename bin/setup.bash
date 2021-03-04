7aaacef255814c870f77e3523449603fa1edb24e
rlnsy <rowlindsay@gmail.com>
2021-03-03 20:32:58 -0800

    switch to script

--- bin/setup.bash
+++ bin/setup.bash
@@ -1,15 +1,29 @@
 #!/bin/bash
+pods=1
+yes=0
+while getopts ":p:" opt; do
+  case ${opt} in
+    p ) pods=$OPTARG
+      ;;
+    y ) yes=1
+      ;;
+    \? ) echo "Usage: setup [-y] [-p <val>]"
+      ;;
+  esac
+done
 echo "Checking node version:"
 node --version
 if [[ $? != 0 ]]; then
     echo "Node not installed!"
     exit 1
 else
-	read -r -p "Is this okay? [Y/n] " confirm
-	if [[ ! $confirm =~ ^[Yy]$ ]]; then	
-		echo "exiting."
-		exit 0
-	fi
+    if [ $yes == 0 ]; then
+        read -r -p "Is this okay? [Y/n] " confirm
+        if [[ ! $confirm =~ ^[Yy]$ ]]; then	
+            echo "exiting."
+            exit 0
+        fi
+    fi
 fi
 yarn install
 ls .env || exit 1
@@ -20,7 +34,9 @@ git clone -b dev-ts https://github.com/HeyMaslo/maslo-persona.git persona
 git clone https://github.com/HeyMaslo/react-native-switch-pro.git
 cd ../..
 yarn all
-cd mobile/ios || exit 1
-pod install --repo-update
-cd ..
+if [ $pods == 1 ]; then
+    cd mobile/ios || exit 1
+    pod install --repo-update
+    cd ../..
+fi
 echo "Done."
