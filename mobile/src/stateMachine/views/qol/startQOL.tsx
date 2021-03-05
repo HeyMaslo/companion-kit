3f22b1eaa761e25649bf1f1e9acf622677b9eb6b
Cara Wong <caraalyssa.wong@gmail.com>
2021-03-05 13:50:39 -0800

    clean up qolStart component

--- mobile/src/stateMachine/views/qol/startQOL.tsx
+++ mobile/src/stateMachine/views/qol/startQOL.tsx
@@ -1,22 +1,11 @@
 import { ViewState } from '../base';
 import React from 'react';
-// import ExpoConstants from 'expo-constants';
 import { observer } from 'mobx-react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import Colors from 'src/constants/colors';
-import { MasloPage, Container, Button, Card, Link } from 'src/components';
-// import AppViewModel from 'src/viewModels';
-// import AppController from 'src/controllers';
+import { StyleSheet, Text, View } from 'react-native';
+import { MasloPage, Container, Button } from 'src/components';
 import { ScenarioTriggers } from '../../abstractions';
-// import Localization from 'src/services/localization';
-// import Images from 'src/constants/images';
 
-import Layout from 'src/constants/Layout';
 import { styles } from 'react-native-markdown-renderer';
-// import { ProfileViewModel } from 'src/viewModels/ProfileViewModel';
-// import { PersonaScrollMask } from 'src/components/PersonaScollMask';
-// import BottomBar from 'src/screens/components/BottomBar';
-// import { TextStyles } from 'src/styles/BaseStyles';
 
 const minContentHeight = 460;
 
@@ -27,84 +16,13 @@ export class qolStartView extends ViewState {
         this._contentHeight = this.persona.setupContainerHeight(minContentHeight, { rotation: -15, transition: { duration: 1.5 } });
     }
 
-    // protected get unbreakable() { return false; }
-
-    async start() {
-        //this.model.notifications.init();
-    }
-
-    //get model() {
-        // return AppViewModel.Instance.Settings;
-    //}
-
-    // private readonly profileVM = new ProfileViewModel();
-
-    componentWillUnmount() {
-        // this.model.notifications.dispose();
-    }
-
-    // logout = () => {
-    //     this.showModal({
-    //         title: 'Are you sure you want to log out?',
-    //         primaryButton: {
-    //             text: 'yes, please',
-    //             action: this.model.logout,
-    //         },
-    //         secondaryButton: {
-    //             text: 'no, I don’t',
-    //             action: null,
-    //         },
-    //     });
-    // }
-
-    // private onPasswordChange = () => {
-    //     this.trigger(ScenarioTriggers.Submit);
-    // }
-
-    // private onEmailChange = () => {
-    //     // this.trigger(ScenarioTriggers.Primary);
-    // }
-
-    // private onNotificationsChange = () => {
-    //     this.trigger(ScenarioTriggers.Primary);
-    // }
+    async start() {}
 
     private cancel = () => {
         this.trigger(ScenarioTriggers.Cancel);
     }
 
-    // private renderLinksFooter() {
-    //     const { feedback, terms, privacy } = Localization.Current.MobileProject.links;
-    //     return (
-    //         <Text style={this.textStyles.p4}>
-    //             { !!feedback ? (
-    //                 <>
-    //                     Send
-    //                     <Link link={feedback}> Feedback, </Link>
-    //                 </>
-    //             ) : null}
-    //             <Text style={feedback ? null : { textTransform: 'capitalize' }}>
-    //                 read
-    //             </Text>
-    //             { !!terms ? (
-    //                 <>
-    //                     <Link link={terms}> Terms <Text style={{ textTransform: 'lowercase' }}>of</Text> Service </Link>
-    //                     or
-    //                 </>
-    //             ) : (
-    //                 <> our</>
-    //             )}
-    //             <Link link={privacy}> Privacy Policy</Link>
-    //         </Text>
-    //     );
-    // }
-
     renderContent() {
-        // const authProviderIcon = this.model.authProvider === 'google'
-        //     ? Images.googleIcon
-        //     : Images.envelopeIcon;
-
-        // const notificationsEnabled = this.model.notifications.isEnabled && !this.model.notifications.isToggleInProgress && this.model.notifications.schedule;
 
         return (
             <MasloPage style={this.baseStyles.page} onClose={this.cancel}>
@@ -117,19 +35,6 @@ export class qolStartView extends ViewState {
         );
     }
 }
-
-// function AppVersionView(this: void) {
-//     const [toggle, setToggle] = React.useState(false);
-
-//     return (
-//         <Text style={[TextStyles.p4, styles.version]} onPress={() => setToggle(!toggle)}>
-//             {toggle
-//                 ? ExpoConstants.installationId
-//                 : AppController.Instance.version.fullVersion
-//             }
-//         </Text>
-//     );
-// }
 
 const styles = StyleSheet.create({ 
     title: {
