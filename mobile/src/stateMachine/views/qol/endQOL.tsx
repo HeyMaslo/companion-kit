58679b757b2e6f5b752e6dc7d2685f07355d73d8
Cara Wong <caraalyssa.wong@gmail.com>
2021-03-20 13:31:53 -0700

    finish skeleton UI of final survey screen

--- mobile/src/stateMachine/views/qol/endQOL.tsx
+++ mobile/src/stateMachine/views/qol/endQOL.tsx
@@ -6,14 +6,16 @@ import { MasloPage, Container, Button } from 'src/components';
 import { ScenarioTriggers } from '../../abstractions';
 
 import { styles } from 'react-native-markdown-renderer';
+import Layout from 'src/constants/Layout';
 
-const minContentHeight = 480;
+const minContentHeight = 1000;
 
 @observer
 export class qolEndView extends ViewState {
     constructor(props) {
         super(props);
-        this._contentHeight = this.persona.setupContainerHeight(minContentHeight, { rotation: -15, transition: { duration: 1.5 } });
+        this._contentHeight = this.persona.setupContainerHeight(minContentHeight, { transition: { duration: 2.2 },
+            position: { x: this.persona.view.position.x, y: Layout.window.height*0.18} });
     }
 
     async start() {}
@@ -30,7 +32,7 @@ export class qolEndView extends ViewState {
 
         return (
             <MasloPage style={this.baseStyles.page} onClose={() => this.cancel()}>
-                <Container style={[{ height: this._contentHeight, paddingTop: 130, alignItems: 'center' }]}>
+                <Container style={[{ height: this._contentHeight, alignItems: 'center' }]}>
                     <Text style={[this.textStyles.h1, styles.title]}>Great job! Here are your Quality of Life results.</Text>
                     <Text style={[this.textStyles.p1, styles.message]}>There are 12 different Life Domains within your overall Quality of Life.</Text>
                     <Button title="CONTINUE" style={styles.readyButton} onPress={() => this.onEndSurvey()}/>
@@ -47,9 +49,9 @@ const styles = StyleSheet.create({
         width: '100%',
     },
     message: {
-        marginTop: 15,
+        marginTop: "100%",
         textAlign: 'center',
-        width: '80%',
+        width: '90%',
         marginBottom: '20%',
     },
     readyButton: {
