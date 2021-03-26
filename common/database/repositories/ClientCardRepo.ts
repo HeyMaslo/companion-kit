6b8497b52bc0b7284ad48fb3af9cce923dd69a31
rlnsy <rowlindsay@gmail.com>
2021-03-26 13:40:44 -0700

    fix lint

--- common/database/repositories/ClientCardRepo.ts
+++ common/database/repositories/ClientCardRepo.ts
@@ -28,11 +28,11 @@ class ClientCardRepo {
     }
 
     async getClientsByEmail(email: string, coachUid?: string, ...statuses: ClientStatus[]) {
-        let query = coachUid
+        const query = coachUid
             ? this.getServerCollection(coachUid)
             : serverOnly(this.db).collectionGroup('clients');
 
-        let qu = query.where('email', '==', email);
+        const qu = query.where('email', '==', email);
 
         const snapshot = await qu.get();
         const result = (snapshot.docs || []).map(d => {
