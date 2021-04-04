462f93ba3ceadd066fe94e7b7c8afb32ced9461f
rlnsy <rowlindsay@gmail.com>
2021-04-04 12:56:50 -0700

    fix lint

--- server/functions/__tests__/example.spec.ts
+++ server/functions/__tests__/example.spec.ts
@@ -1,19 +1,20 @@
 import * as admin from 'firebase-admin';
 import { fail } from 'assert';
-const { expect } = require("chai");
+import { expect } from 'chai';
+import fbFuncTest from 'firebase-functions-test';
 
 // Initialize testing server
-const test = require("firebase-functions-test")({
+const test = fbFuncTest({
     projectId: 'bipolarbridges',
   });
 admin.initializeApp();
 
-describe("Example Tests", () => {
-	afterEach(async () => {
-		await test.cleanup();
-	});
-	it("Should check that tests work", () => {
-		const result = 1 + 1;
-		expect(result).to.equal(2);
-	});
+describe('Example Tests', () => {
+    afterEach(async () => {
+        await test.cleanup();
+    });
+    it('Should check that tests work', () => {
+        const result = 1 + 1;
+        expect(result).to.equal(2);
+    });
 });
