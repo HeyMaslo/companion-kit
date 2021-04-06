440e80b7daa90d393918bc6dc8c34ef7cdfc1de4
rlnsy <rowlindsay@gmail.com>
2021-04-06 00:11:20 -0700

    wrap firebase init in helper

--- server/functions/__tests__/example.spec.ts
+++ server/functions/__tests__/example.spec.ts
@@ -1,17 +1,13 @@
-import * as admin from 'firebase-admin';
 import { fail } from 'assert';
 import { expect } from 'chai';
-import fbFuncTest from 'firebase-functions-test';
+
+import { init } from './util/firebase';
 
 // should be able to import things like the following:
 import { initializeAsync } from '../../../common/services/firebase';
 import { env } from '../../../env';
 
-// Initialize testing server
-const test = fbFuncTest({
-    projectId: 'bipolarbridges',
-  });
-admin.initializeApp();
+const test = init('example-test');
 
 describe('Example Tests', () => {
     afterEach(async () => {
