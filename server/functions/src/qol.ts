98362a9c3b873b927a16c589b39f72c83a7fa2dc
rlnsy <rowlindsay@gmail.com>
2021-04-06 18:59:39 -0700

    impl

--- server/functions/src/qol.ts
+++ server/functions/src/qol.ts
@@ -1,29 +1,62 @@
-type createDomainArgs = {
+import { DomainScope, Domain } from 'common/models/QoL';
+import RepoFactory from 'common/controllers/RepoFactory';
+import { Identify } from 'common/models';
+
+type CreateDomainArgs = {
     scope:      string,
     position:   number,
     name:       string,
     slug:       string,
 };
 
-type createResult = {
-    error: string | null,
+type Error = string | null;
+
+type CreateResult = {
+    error: Error,
 };
 
-export async function createDomain(args: createDomainArgs)
-    : Promise<createResult> {
-    return {
-        error: null
-    }; // STUB
+type GetManyResult<T> = {
+    error: Error,
+    results: T[]
 }
 
-type createQuestionArgs = {
+export async function createDomain(args: CreateDomainArgs)
+    : Promise<CreateResult> {
+    
+    if (args.scope in DomainScope) {
+        await RepoFactory.Instance.domains.create({
+            scope:      args.scope as DomainScope,
+            position:   args.position,
+            name:       args.name,
+            slug:       args.slug,
+        })
+        return {
+            error: null
+        };
+    } else {
+        return {
+            error: "Invalid scope"
+        }
+    }
+}
+
+export async function getDomains()
+    : Promise<GetManyResult<Identify<Domain>>> {
+
+    return {
+        error: null,
+        results: await RepoFactory.Instance.domains.get()
+    };
+}
+
+type CreateQuestionArgs = {
     text: string,
     domainSlug: string,
     position: number,
 };
 
-export async function createQuestion(args: createQuestionArgs)
-    : Promise<createResult> {
+export async function createQuestion(args: CreateQuestionArgs)
+    : Promise<CreateResult> {
     return {
         error: null
     }; // STUB
