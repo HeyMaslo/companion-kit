4ddbb49292c18ebea942d1a5c0fc476d62dc5eb2
rlnsy <rowlindsay@gmail.com>
2021-04-12 12:20:47 -0700

    more client mocks

---
+++ server/functions/__tests__/mocks/client/controllers.ts
@@ -0,0 +1,22 @@
+import AuthControllerBase from '../../../../../common/controllers/AuthController';
+import StorageMock from './storage';
+
+export class ClientAuthController extends AuthControllerBase {
+
+    get targetRole(): any {
+        throw new Error('Method not implemented.');
+    }
+    get locationUrl(): string {
+        throw new Error('Method not implemented.');
+    }
+    protected get Storage(): any {
+        return StorageMock;
+    }
+    signInWithEmailLink(email: string, reason: any): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+    protected googleSignOut(): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+
+}
