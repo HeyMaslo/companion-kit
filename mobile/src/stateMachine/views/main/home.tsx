68c38b994f38c79ff4d20f7c7ab0dc4385cf82ef
Cara Wong <47067364+cara-wong@users.noreply.github.com>
2021-04-17 12:01:14 -0700

    fix bugs on logout and login again

--- mobile/src/stateMachine/views/main/home.tsx
+++ mobile/src/stateMachine/views/main/home.tsx
@@ -47,11 +47,9 @@ export class HomeView extends ViewState<{ opacity: Animated.Value, isUnfinishedQ
     get viewModel() { return HomeViewModel.Instance; }
 
     async start() {
-        if (isFirstLaunch) {
-            await AppViewModel.Instance.QOL.init();
-            const mags = await this.viewModel.getArmMagnitudes();
-            this.persona.qolMags = mags;
-        }
+        await AppViewModel.Instance.QOL.init();
+        const mags = await this.viewModel.getArmMagnitudes();
+        this.persona.qolMags = mags;
         this.setState({...this.state, isUnfinishedQol: AppViewModel.Instance.QOL.isUnfinished});
         Animated.timing(this.state.opacity, {
             toValue: 1,
