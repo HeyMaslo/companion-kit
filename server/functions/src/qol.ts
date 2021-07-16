024e94c3f95bb028e726628f1933dce5a8d910ad
Mark Davies <71405899+MarkDavies0@users.noreply.github.com>
2021-07-16 13:20:38 -0700

    Update qol.ts

--- server/functions/src/qol.ts
+++ server/functions/src/qol.ts
@@ -35,7 +35,7 @@ export const QoLEndpoint = new FunctionFactory(QoLFunctions.QoLEndpoint)
 export async function createDomain(args: CreateDomainRequest)
     : Promise<CreateDomainResponse> {
 
-    if (!(args.name)) {
+    if (!(args.name in DomainName)) {
         return {
             error: 'Invalid name',
         };
