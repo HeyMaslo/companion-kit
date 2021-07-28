2930db35257f24fb690508ec07de0a2594748079
SukiCai <yufeicaimail@gmail.com>
2021-07-28 15:44:50 -0700

    added skip button in healthConsent

--- mobile/src/stateMachine/views/healthData/healthConsent.tsx
+++ mobile/src/stateMachine/views/healthData/healthConsent.tsx
@@ -3,13 +3,6 @@ import { ViewState } from '../base';
 import AppController from 'src/controllers';
 import { ScenarioTriggers } from '../../abstractions';
 import logger from 'common/logger';
-import React from 'react';
-import { styles } from 'react-native-markdown-renderer';
-import { View } from 'react-native';
-import { Button, Container, MasloPage} from 'src/components';
-import Images from 'src/constants/images';
-import BottomBar from 'src/screens/components/BottomBar';
-import { PersonaScrollMask } from 'src/components/PersonaScollMask';
 
 
 @observer
@@ -22,29 +15,23 @@ export class HealthConsentView extends ViewState {
                 text: 'Continue',
                 action: this.askNtfPermissions,
             },
+            secondaryButton: {
+                text: 'Skip for now',
+                action: this.skip,
+            },
         });
     }
 
     askNtfPermissions = () => this.runLongOperation(async () => {
     const enabled = await AppController.Instance.User.hasHealthDataPermissions.askPermission();
       logger.log("in ASKNOTIFICATIONS : ", enabled);
-      this.trigger(ScenarioTriggers.Submit);
+      this.trigger(ScenarioTriggers.Primary);
   })
 
+    skip () {
+      this.trigger(ScenarioTriggers.Submit);
+    }
+
   renderContent() {
-    return (
-        <MasloPage style={this.baseStyles.page}>
-            <Container style={styles.topBarWrapWrap}>
-                <PersonaScrollMask />
-                {!process.appFeatures.GOALS_ENABLED &&
-                    <View style={styles.topBarWrap}>
-                        <Button style={styles.backBtn} underlayColor="transparent" onPress={() => this.trigger(ScenarioTriggers.Back)}>
-                            <Images.backIcon width={28} height={14} />
-                        </Button>
-                    </View>
-                }
-            </Container>
-            {process.appFeatures.GOALS_ENABLED && <BottomBar screen={'settings'} />}
-            </MasloPage>
-    );
+    return null;
 }}
