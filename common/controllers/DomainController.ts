65f88a633f161a6110b367a797c6bc7635b737e6
Mark Davies <71405899+MarkDavies0@users.noreply.github.com>
2021-08-03 11:30:10 -0700

    update userstate, domaincontroller, strategycontroller

--- common/controllers/DomainController.ts
+++ common/controllers/DomainController.ts
@@ -1,13 +1,21 @@
 import { IDomainController } from 'common/abstractions/controlllers/IDomainController';
-import { DomainIded } from '../../mobile/src/constants/Domain';
+import { DomainIded, DomainName } from '../../mobile/src/constants/Domain';
 import RepoFactory from 'common/controllers/RepoFactory';
 
 export default class DomainControllerBase implements IDomainController {
 
-    public async getPossibleDomains(): Promise<DomainIded[]> {
-        return await RepoFactory.Instance.qolDomains.get();
-      }
+  private _userId: string;
 
+  public setUser(userId: string) {
+    this._userId = userId;
+  }
 
+  public async getPossibleDomains(): Promise<DomainIded[]> {
+    return await RepoFactory.Instance.qolDomains.get();
+  }
+
+  public async getFocusedDomains(): Promise<DomainName[]> {
+    return (await RepoFactory.Instance.userState.getByUserId(this._userId)).focusedDomains;
+  }
 
 }
