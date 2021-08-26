397a4fc2c054874592022c057abe97f2fb538f5a
Mark Davies <71405899+MarkDavies0@users.noreply.github.com>
2021-08-26 13:01:42 -0700

    lint

--- mobile/src/stateMachine/views/lifeDomains/chooseDomain.tsx
+++ mobile/src/stateMachine/views/lifeDomains/chooseDomain.tsx
@@ -9,6 +9,7 @@ import { ViewDomainsBase } from './viewDomainsBase';
 
 @observer
 export class ChooseDomainView extends ViewDomainsBase {
+
     constructor(props) {
         super(props);
         this._contentHeight = this.persona.setupContainerHeightForceScrollDown({ rotation: -15, transition: { duration: 1 }, scale: 1.2 });
