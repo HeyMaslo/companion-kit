c49fbda7c604be2e9f6e42d62d596f6768490f8a
Rowan Lindsay <rowan@rowanlindsay.com>
2021-09-11 15:14:30 -0700

    fix status codes

--- bin/nvm.sh
+++ bin/nvm.sh
@@ -1,14 +1,14 @@
-ok=1
+status=0
 if [ ! -f "$(pwd)/bin/setup.bash" ]; then
 	echo "Please source this file from the project root"
-	ok=0
+	status=1
 elif [ ! -d "./.nvm" ]; then
-	ok=0
+	status=1
 	bash ./bin/setup.bash -n -y
-	ok=$?
+	status=$?
 fi
 
-if [ $ok != 0 ]; then
+if [ $status != 0 ]; then
 	echo "Sorry, this didn't work properly. Please try again."
 else
 	export NVM_DIR="$(pwd)/.nvm"
