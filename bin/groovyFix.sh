8a591822f8554c16bd5055fe94736fe876a447cd
Mark Davies <71405899+MarkDavies0@users.noreply.github.com>
2021-09-20 09:54:46 -0700

    Update groovyFix.sh

--- bin/groovyFix.sh
+++ bin/groovyFix.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i '' 's/\*\*\/MainApplication.java/\*\*\/main\/\*\*\/MainApplication.java/g' mobile/node_modules/react-native-unimodules/gradle.groovy
-sed -i '' 's/\*\*\/MainApplication.kt/\*\*\/main\/\*\*\/MainApplication.kt/g' mobile/node_modules/react-native-unimodules/gradle.groovy
+sed -i 's/\*\*\/MainApplication.java/\*\*\/main\/\*\*\/MainApplication.java/g' mobile/node_modules/react-native-unimodules/gradle.groovy
+sed -i 's/\*\*\/MainApplication.kt/\*\*\/main\/\*\*\/MainApplication.kt/g' mobile/node_modules/react-native-unimodules/gradle.groovy
