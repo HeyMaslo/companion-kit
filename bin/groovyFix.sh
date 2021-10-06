3c78070a07b85acdc047235b5209b60aff006aff
Mark Davies <71405899+MarkDavies0@users.noreply.github.com>
2021-10-06 12:55:57 -0700

    finish fix script

--- bin/groovyFix.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# sed -i 's/\*\*\/MainApplication.java/\*\*\/main\/\*\*\/MainApplication.java/g' mobile/node_modules/react-native-unimodules/gradle.groovy
-# sed -i 's/\*\*\/MainApplication.kt/\*\*\/main\/\*\*\/MainApplication.kt/g' mobile/node_modules/react-native-unimodules/gradle.groovy
-
-ls ./mobile/android/app/src/main/java/com/companionkit
