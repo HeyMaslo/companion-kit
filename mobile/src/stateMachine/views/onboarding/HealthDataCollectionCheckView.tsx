97230a9ff450fd39e5b5787d752356dac38ce4fe
Mark Davies <71405899+MarkDavies0@users.noreply.github.com>
2022-02-10 22:49:36 -0800

    update copy for android

--- mobile/src/stateMachine/views/onboarding/HealthDataCollectionCheckView.tsx
+++ mobile/src/stateMachine/views/onboarding/HealthDataCollectionCheckView.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import { ViewState } from '../base';
 import AppController from 'src/controllers';
 import { ScenarioTriggers } from '../../abstractions';
@@ -21,7 +21,7 @@ export class HealthDataCollectionCheckView extends ViewState {
       <MasloPage style={this.baseStyles.page} theme={this.theme}>
         <Container style={[{ height: '100%', flexDirection: 'column', alignItems: 'center', paddingTop: 60 }]}>
           <Text style={[TextStyles.h1, styles.title, { color: this.theme.colors.foreground }]}>{'Is your device capturing all your health data?'}</Text>
-          <Text style={[TextStyles.h2, styles.title, { color: this.theme.colors.foreground }]}>{'If you are using any other apps that track your mood, sleep, or other health information, make sure you have given those apps permission to send your health metrics over to Apple Health. '}</Text>
+          <Text style={[TextStyles.h2, styles.title, { color: this.theme.colors.foreground }]}>{`If you are using any other apps that track your mood, sleep, or other health information, make sure you have given those apps permission to record your health metrics in ${Platform.OS == 'ios' ? 'Apple Health' : 'Google Fit'}.`}</Text>
           <Button
             title='yes, continue'
             style={{ marginTop: 'auto' }}
